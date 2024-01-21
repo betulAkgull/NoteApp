7754568a24813f394db3349b6a9acb01088fa3f9
Betül Akgül <76072632+betulAkgull@users.noreply.github.com>
2024-01-21 23:01:04 +0300

    lazyColumn at homepage added

--- app/src/main/java/com/example/noteapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/noteapp/ui/home/HomeScreen.kt
@@ -1,24 +1,30 @@
 package com.example.noteapp.ui.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -28,6 +34,7 @@ import com.example.noteapp.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
+    val notes = List(100) { "Note number ${it+1}" }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -35,25 +42,10 @@ fun HomeScreen(navController: NavController) {
         contentAlignment = Alignment.TopStart,
     ) {
         Text(text = "Notes", fontSize = 32.sp, color = MaterialTheme.colorScheme.surface)
-        Image(
-            modifier = Modifier
-                .align(Alignment.Center),
-            painter = painterResource(id = R.drawable.rafiki),
-            contentDescription = "No Notes Image"
-        )
-        Text(
-            modifier = Modifier
-                .padding(top = 120.dp)
-                .align(Alignment.Center),
-            text = "Create your first note !",
-            color = Color.White
-        )
-        Button(
-            onClick = { navController.navigate("Detail") },
-            modifier = Modifier.align(Alignment.BottomCenter),
-            shape = RoundedCornerShape(20.dp)
-        ) {
-            Text(text = "Go to Detail")
+        if (notes.isNotEmpty()) {
+            NotesList(notes = notes, navController)
+        } else {
+            EmptyList()
         }
         Fab(navController)
     }
@@ -76,6 +68,68 @@ fun Fab(navController: NavController) {
         Icon(
             imageVector = Icons.Rounded.Add,
             contentDescription = "Fab Button",
+        )
+    }
+}
+
+@Composable
+fun NotesList(notes: List<String>, navController: NavController) {
+    LazyColumn(modifier = Modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp)) {
+        items(notes) { note ->
+            Note(note = note, navController)
+        }
+    }
+}
+
+@Composable
+fun Note(note: String, navController: NavController) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(MaterialTheme.colorScheme.surface)
+            .clickable {
+                navController.navigate("Detail")
+            },
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+
+        ) {
+            Text(
+                text = note,
+                style = MaterialTheme.typography.bodyLarge,
+                color = contentColorFor(MaterialTheme.colorScheme.surface),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun EmptyList() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.TopStart,
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.Center),
+            painter = painterResource(id = R.drawable.rafiki),
+            contentDescription = "No Notes Image"
+        )
+        Text(
+            modifier = Modifier
+                .padding(top = 120.dp)
+                .align(Alignment.Center),
+            text = "Create your first note !",
+            color = Color.White
         )
     }
 }
